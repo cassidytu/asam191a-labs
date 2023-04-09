73a3ef9caff30467a58b33f03b1028902ae7d429
cassidytu <cassidytu@ucla.edu>
2023-04-09 16:58:53 -0700

    update

--- week 1/js/init.js
+++ week 1/js/init.js
@@ -1,5 +1,5 @@
 // JavaScript const variable declaration
-const map = L.map('the_map').setView([34.095287, -118.127014], 15); // (1)!
+const map = L.map('the_map').setView([34.095287, -118.127014], 10); // (1)!
 
 // Leaflet tile layer, i.e. the base map
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -12,17 +12,17 @@ let marker1 = L.marker([34.094620, -118.1267235]).addTo(map) // (3)!
 		.openPopup();
 
 let marker2 = L.marker([34.083280, -118.073870]).addTo(map) // (3)!
-		.bindPopup("<b>Rose City Pizza in Rosemead, CA<b> <br> 3588 Rosemead Blvd, Rosemead, CA 91770 <br> I bet you've never had pizza like this, this pizzeria offers unique toppings based off of neighboring cuisines! I recommend getting a half Island Shrimp and half Birria or Taiwanese Popcorn Chicken pizza.")
+		.bindPopup('<b>Rose City Pizza in Rosemead, CA<b> <br> 3588 Rosemead Blvd, Rosemead, CA 91770 <br> I bet you have never had pizza like this, this pizzeria offers unique toppings based off of neighboring cuisines! I recommend getting a half Island Shrimp and half Birria or Taiwanese Popcorn Chicken pizza.')
 		.openPopup();
 
-let marker3 = L.marker([34.083280, -118.073870]).addTo(map) // (3)!
-		.bindPopup("<b>Neighborhood Tea House in Rosemead, CA<b> <br> 4213 Rosemead Blvd h2, Rosemead, CA 91770 <br> Not food, but the Vietnamese inspired drinks here are refreshing and one of a kind! I recommend getting the signature Smashed Avocado Durian and Cocotaro drinks.")
+let marker3 = L.marker([34.0832, -118.07386]).addTo(map) // (3)!
+		.bindPopup('<b>Neighborhood Tea House in Rosemead, CA<b> <br> 4213 Rosemead Blvd h2, Rosemead, CA 91770 <br> Not food, but the Vietnamese inspired drinks here are refreshing and one of a kind! I recommend getting the signature Smashed Avocado Durian and Cocotaro drinks.')
 		.openPopup();
 
 let marker4 = L.marker([34.102150, -118.112080]).addTo(map) // (3)!
-		.bindPopup("<b>Tasty Noodle House in San Gabriel, CA<b> <br> 827 W Las Tunas Dr San Gabriel, CA 91776 <br> This Chinese restaurant is another comfort spot that has solid food at cheap prices! I recommend the Pan-fried Pork Buns and House Beef Noodle Soup.")
+		.bindPopup('<b>Tasty Noodle House in San Gabriel, CA<b> <br> 827 W Las Tunas Dr San Gabriel, CA 91776 <br> This Chinese restaurant is another comfort spot that has solid food at cheap prices! I recommend the Pan-fried Pork Buns and House Beef Noodle Soup.')
 		.openPopup();
 
 let marker5 = L.marker([34.046170, -118.161490]).addTo(map) // (3)!
-		.bindPopup("<b>Carnitas El Momo in Monterey Park, CA<b> <br> 1470 Monterey Pass Rd, Monterey Park, CA 91754 <br> Featured in the Netflix Street Food Series, this Mexican spot is a staple for everyone in the area. I recommend the usual Michoacan-style carnitas tacos and the Cucumber-Lime agua fresca.")
+		.bindPopup('<b>Carnitas El Momo in Monterey Park, CA<b> <br> 1470 Monterey Pass Rd, Monterey Park, CA 91754 <br> Featured in the Netflix Street Food Series, this Mexican spot is a staple for everyone in the area. I recommend the usual Michoacan-style carnitas tacos and the Cucumber-Lime agua fresca.')
 		.openPopup();
